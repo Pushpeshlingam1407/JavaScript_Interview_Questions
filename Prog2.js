381cc0dbbb28f1fd529f40d48dad614eda0efb70
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-07-20 15:48:06 +0530

    Added object property access methods

--- Prog2.js
+++ Prog2.js
@@ -9,4 +9,9 @@ const person = {
   },
 };
 
-person.greet()
+person.greet();
+
+console.log(Object.keys(person)); //* Returns an array of the object's keys
+console.log(Object.values(person)); //* Returns an array of the object's values
+console.log(Object.entries(person)); //* Returns an array of the object's key-value pairs
+
