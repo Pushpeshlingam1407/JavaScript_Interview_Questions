0c78cbe268b8e09ae6c9a544972dd34e8d1784a1
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-07-20 15:49:46 +0530

    Added object manipulation and inheritance example

--- Prog2.js
+++ Prog2.js
@@ -15,3 +15,7 @@ console.log(Object.keys(person)); //* Returns an array of the object's keys
 console.log(Object.values(person)); //* Returns an array of the object's values
 console.log(Object.entries(person)); //* Returns an array of the object's key-value pairs
 
+const student = Object.create(person); //* Creates a new object with the specified prototype object and properties
+student.name = "Jon Snow";
+student.greet(); //* Inherited method from person object
+console.log(student); //* Logs the student object with its own properties and inherited properties from person object
