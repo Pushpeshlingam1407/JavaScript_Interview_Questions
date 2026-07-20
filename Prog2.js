bf31730e73df5c75c1eb8773d95281be851002a2
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-07-20 15:46:41 +0530

    Added greet method to person object

---
+++ Prog2.js
@@ -0,0 +1,15 @@
+//TODO:
+
+const person = {
+  id: 1,
+  name: "Pushpesh",
+  age: 22,
+  greet: function () {
+    console.log("Hello, my name is " + this.name);
+  },
+};
+
+person.greet();
+
+//Correct git commit message: "Added greet method to person object"
+
