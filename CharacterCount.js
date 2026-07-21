f5663f0572d5f48edbe3a64f6155c2c449218a2e
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-07-21 16:05:50 +0530

    docs: add explanatory comments and formatting to CharacterCount.js and ApplyMethod.js

--- CharacterCount.js
+++ CharacterCount.js
@@ -1,3 +1,6 @@
+//TODO: Count the number of occurrences of each character in a string
+
+// * Method 1: Using for loop
 const name = "Pushpesh.Lingam";
 let cnt = {};
 for (let i = 0; i < name.length; i++) {
@@ -10,21 +13,21 @@ for (let i = 0; i < name.length; i++) {
 }
 console.log(cnt);
 
-//Method 2: Using reduce method
+// * Method 2: Using reduce method
 let count = name.split("").reduce((acc, ch) => {
   acc[ch] = (acc[ch] || 0) + 1;
   return acc;
 }, {});
 console.log(count);
 
-//Method 3: Using forEach method
+// * Method 3: Using forEach method
 let countForEach = {};
 name.split("").forEach((ch) => {
   countForEach[ch] = (countForEach[ch] || 0) + 1;
 });
 console.log(countForEach);
 
-//Method 4: Using Map
+// * Method 4: Using Map
 let countMap = new Map();
 name.split("").forEach((ch) => {
   countMap.set(ch, (countMap.get(ch) || 0) + 1);
