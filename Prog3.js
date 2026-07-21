7eaabd937d26b6740d885f7e89f138cfb8fa467c
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-07-21 15:45:28 +0530

    Added character count methods using for loop and reduce

--- Prog3.js
+++ Prog3.js
@@ -10,8 +10,18 @@ for (let i = 0; i < name.length; i++) {
 }
 console.log(cnt);
 
-// //Method 2: Using reduce method
-// let count = name.split("").reduce((acc, ch) => {
-//   acc[ch] = (acc[ch] || 0) + 1;
-//   return acc;
-// }, {});
+//Method 2: Using reduce method
+let count = name.split("").reduce((acc, ch) => {
+  acc[ch] = (acc[ch] || 0) + 1;
+  return acc;
+}, {});
+console.log(count);
+
+// //Method 3: Using forEach method
+// let countForEach = {};
+// name.split("").forEach((ch) => {
+//   countForEach[ch] = (countForEach[ch] || 0) + 1;
+// });
+// console.log(countForEach);
+
+//commit message: "Added character count methods using for loop, reduce, and forEach."
